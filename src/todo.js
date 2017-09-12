4dacf700e3c6b0e7e932f11765f8dd01ff8f0d3a
ngvanessa <Ga1axies@Vanessas-MacBook-Pro.local>
2017-09-12 15:09:52 -0700

    day 1

---
+++ src/todo.js
@@ -0,0 +1,57 @@
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str != null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+}
+
+    // retrieves item 
+function add() {
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+    
+    
+    //ends action
+    return false;
+}
+
+//shows function
+
+function show () {
+    var todos = get_todos();
+    
+    var html = '<ul>';
+    for (var ii=0; ii<todos.length; ii++) {
+        html += '<li>' + todos [ii] + '<button class="remove" id=" ' + ii + '"> x </button> </li> ; 
+        
+    } ;
+    
+    html += '</ul>';
+    
+    document.getElementById('todos').innerHTML = html;
+    
+    var buttons = document.getElementsByClassName('remove');
+    for (var ii=0; ii <buttons.length; ii++) {
+        buttons[ii].addEventListener ('click', remove);
+    };
+    
+    // removes element and stores the new list in the database
+    
+    function remove () {
+        var id= this.getAttribute('id');
+        var todos = get_todos();
+        todos.splice(id, 1);
+        localStorage.setItem('todo', JSON.stringify(todos));
+        
+        show();
+        
+        return false;
+    }
+}
